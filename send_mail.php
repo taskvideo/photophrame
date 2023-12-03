691a09584584ca5130d89dc2c3f0ccc64d112fca
taskvideo <145209758+taskvideo@users.noreply.github.com>
2023-12-03 12:00:59 +0100

    Update send_mail.php
    test email 3

--- send_mail.php
+++ send_mail.php
@@ -1,12 +1,14 @@
 <?php
 $mail_to = "info@francocionini.it";
-$mail_from = "postmaster@francocionini.it";
+$mail_from = "postmaster@francocionini.it"; // Set this to the email address you want to appear as the sender
 
-/**
-$mail_to = "yourpersonalemail@gmail.com";
-$mail_from = "webmaster@example.com";
-**/
+// Other email configuration settings
+$imap_server = "imaps.aruba.it";
+$imap_port = 993;
+$smtp_server = "smtps.aruba.it";
+$smtp_port = 465;
 
+// Your existing email content and headers
 $name = strip_tags(trim($_POST['name']));
 $email = strip_tags(trim($_POST['email']));
 $subject = strip_tags(trim($_POST['subject']));
@@ -21,9 +23,10 @@ $message .= "<b>Message:</b> ".$text."<br>";
 
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-// More headers
 $headers .= "From: <".$mail_from.">" . "\r\n";
 
 mail($mail_to,$mail_subject,$message,$headers);
 ?>
+
+
+
